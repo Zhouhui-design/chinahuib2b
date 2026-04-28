09359f1d53f409729fe00dd1b7e25f42530e029b
Zhouhui <1994169577@qq.com>
2026-04-29 04:33:24 +0800

    fix: remove conflicting static robots.txt, keep dynamic route only

---
+++ src/app/test-components/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import AnnouncementBar from '@/components/AnnouncementBar'
+import DisclaimerTicker from '@/components/DisclaimerTicker'
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Components</h1>
+      <AnnouncementBar />
+      <DisclaimerTicker />
+    </div>
+  )
+}
